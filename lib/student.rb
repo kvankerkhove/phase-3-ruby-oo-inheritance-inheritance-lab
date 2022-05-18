6f487637594c56f6e2cee2b5ac789de65994be15
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-18 16:22:10 -0400

    commit tree 0689d38532bb91578c37f92d62650533745ad14e

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn (string_of_knowledge)
+        @knowledge << string_of_knowledge
+    end
 
 end
